9a992b0e2a30e578d31c689c6de203b78f7b6187
zhang <zz593141477@gmail.com>
2015-02-13 11:24:29 +0800

    card reader

--- firmware/README.md
+++ firmware/README.md
@@ -7,3 +7,4 @@ Library安装方法(Arduino对库的支持实在太hack了)：
 1. 参照WIZ_Ethernet_Library-master中的说明，替换Arduino自带的Ethernet库，以支持W5500
 2. Select device部分选择`W5500_ETHERNET_SHIELD`
 3. 注释掉 `#define WIZ550io_WITH_MACADDRESS`
+4. 参照PN532-master中的说明将`PN532*`文件夹复制到Arduino的库目录中
--- firmware/core/core.ino
+++ firmware/core/core.ino
@@ -1,8 +1,13 @@
 #include <SPI.h>
 #include <Ethernet.h>
+#include <Wire.h>
+#include <PN532_I2C.h>
+#include "PN532.h"
+
 #include "protocol.h"
 #include "common.h"
 #include "peripheral.h"
+
 
 const int led = 13; //LED pin config
 byte mac[] = {0xDE, 0xAD, 0x00, 0x09, 0x00, 0x09};
@@ -15,6 +20,9 @@ EthernetClient client;
 Door door;
 Alarm alarm;
 Button button;
+
+static PN532_I2C pn532_i2c(Wire);
+static PN532 pn532(pn532_i2c);
 
 void software_Reset() // Restarts program from beginning but does not reset the peripherals and registers
 {
@@ -41,6 +49,30 @@ void ErrorConnect() //Indicates connecting error occurred
   }
 }
 
+void initRfid()
+{
+  pn532.begin();
+
+  uint32_t versiondata = pn532.getFirmwareVersion();
+  if (! versiondata) {
+    Serial.println("Didn't find PN53x board");
+    return;
+  }
+
+  // Got ok data, print it out!
+  Serial.print("Found chip PN5"); Serial.print((versiondata>>24) & 0xFF, HEX); 
+  Serial.print(" with firmware ver. "); Serial.print((versiondata>>16) & 0xFF, DEC); 
+  Serial.print('.'); Serial.println((versiondata>>8) & 0xFF, DEC);
+
+  // Set the max number of retry attempts to read from a card
+  // This prevents us from waiting forever for a card, which is
+  // the default behaviour of the PN532.
+  pn532.setPassiveActivationRetries(0xFF);
+  
+  // configure board to read RFID tags
+  pn532.SAMConfig();
+}
+
 void setup() {
   pinMode(led, OUTPUT);
   door.Init();
@@ -53,6 +85,9 @@ void setup() {
   while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
+  Serial.println("Card9 controller started");
+
+  initRfid();
 
   // start the Ethernet connection:
   Serial.println("Trying to get an IP address using DHCP");
@@ -139,11 +174,50 @@ void renewDhcp()
   }
 }
 
+void findRfidCard()
+{
+  boolean success;
+  static uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
+  static uint8_t uidLength = 0;                    // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
+  static unsigned long previousMillis = 0;
+
+  
+  unsigned long currentMillis = millis();
+  if(currentMillis - previousMillis > 300){
+    previousMillis = currentMillis;
+  }else{
+    return;
+  }
+
+  // Wait for an ISO14443A type cards (Mifare, etc.).  When one is found
+  // 'uid' will be populated with the UID, and uidLength will indicate
+  // if the uid is 4 bytes (Mifare Classic) or 7 bytes (Mifare Ultralight)
+  success = pn532.readPassiveTargetID(PN532_MIFARE_ISO14443A, &uid[0], &uidLength, 10);
+  
+  if (success) {
+    Serial.println("Found a card!");
+    Serial.print("UID Length: ");Serial.print(uidLength, DEC);Serial.println(" bytes");
+    Serial.print("UID Value: ");
+    for (uint8_t i=0; i < uidLength; i++) 
+    {
+      Serial.print(" 0x");Serial.print(uid[i], HEX); 
+    }
+    Serial.println("");
+    // Wait 1 second before continuing
+    // delay(1000);
+  }
+  else
+  {
+    // PN532 probably timed out waiting for a card
+    Serial.println("Timed out waiting for a card");
+  }
+}
+
 void loop() {
   static uint8_t connect_retries = 0;
 
   button.Check();
-  
+
   if(door.UpdateState()){
     alarm.On();
   }
@@ -154,6 +228,8 @@ void loop() {
   }else if(a == Button::ActionLongPressed){
     alarm.Off();
   }
+
+  findRfidCard();
 
   if (!client.connected()) {
     digitalWrite(led, LOW);
