df34f278a07385daa9707b7c3b8d21b0d6723c47
王邈 <shankerwangmiao@gmail.com>
2015-02-14 22:32:07 +0800

    添加与新A9的智能卡模块的接口

--- software/auth/cardAuth.js
+++ software/auth/cardAuth.js
@@ -1,3 +1,32 @@
+var config = require("../config")
+var request = require("request")
+
 module.exports = function(fingerPrint, callback){
-	callback(null, null);
+	request.post({
+		url: config.accounts9 + "/interface/getUserByIdentity",
+		json: {
+			interfaceSecret: config.interfaceSecret,
+			identity: fingerPrint
+		},
+		strictSSL: true,
+	}, function(error, response, body){
+		if(error){
+			return callback(error);
+		}
+		if(body.error === 'no-such-user'){
+			return callback(null, null);
+		}else if(body.error){
+			return callback(body.error, null);
+		}else{
+			if(body.user.groups.some(function(g){
+				return config.acceptGroup.some(function(h){
+					return h === g;
+				});
+			})){
+				return callback(null, body.user.name);
+			}else{
+				return callback(null, null);
+			}
+		}
+	});
 }
--- software/config.example.js
+++ software/config.example.js
@@ -4,6 +4,7 @@ module.exports = {
 	logFile    : "./access.log",
 	sockFile   : "/tmp/card9.sock",
 	accounts9  : "https://accounts.net9.org",
-  setUid      : "nobody",
+  setUid     : "", //should not be root; should have access to the source code
 	interfaceSecret : "Example",
+	acceptGroup: ["access-door"],
 };
